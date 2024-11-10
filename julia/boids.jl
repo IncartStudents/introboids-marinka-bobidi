bab141e7fc8c11375aa0a36baca5fc232ff4ba1f
marinka-bobidi <155455955+marinka-bobidi@users.noreply.github.com>
2024-11-10 22:26:49 +0300

    merging func

--- julia/boids.jl
+++ julia/boids.jl
@@ -3,178 +3,163 @@ using Plots
 
 mutable struct WorldState
     boids::Vector{Tuple{Float64, Float64}}
-    velocities::Vector{Tuple{Float64, Float64}}
+    vec_vel::Vector{Tuple{Float64, Float64}}
+    vec_accel::Vector{Tuple{Float64, Float64}}
     height::Float64
     width::Float64
-    radius::Float64
-    function WorldState(n_boids, h, w)
-        boids = [(rand(0:w), rand(0:h)) for _ in 1:n_boids]
-        speed = 2
-        velocities = [(rand(-1.0:0.1:1.0) * speed, rand(-1.0:0.1:1.0) * speed) for _ in 1:n_boids]
-        new(boids, velocities, h, w)
+    max_vel::Float64
+    min_r::Float64
+    max_r::Float64
+    factor_center::Float64
+    factor_sep::Float64
+    function WorldState(n_boids, h, w, max_vel, min_r, max_r, factor_center, factor_sep)
+        boids = [(rand() * w, rand() * h) for _ in 1:n_boids]
+        vec_vel = [(rand(-1.0:0.1:1.0) * max_vel * 2, rand(-1.0:0.1:1.0) * max_vel * 2) for _ in 1:n_boids]
+        vec_accel = [(0.0, 0.0) for _ in 1:n_boids]
+        new(boids, vec_vel, vec_accel, h, w, max_vel, min_r, max_r, factor_center, factor_sep)
     end
 end
 
-# Функция движения в одном направлении
-function alignment(state::WorldState, i::Int, max_radius::Int)
-    sum_velocity_x = 0.0
-    sum_velocity_y = 0.0
+# Функция для расчета отталкивания от ближайших соседей
+function alignment(state::WorldState, i::Int)
+    sum_velocity = (0.0, 0.0)
     neighbor_count = 0
+    boid_x, boid_y = state.boids[i]
+    vel_x, vel_y = state.vec_vel[i]
 
-    position = state.boids
-    velocities = state.velocities
-    boids_velocity = velocities[i]
-
-    for j in 1:length(position)
+    for j in 1:length(state.boids)
         if i != j
-            pos_i_x, pos_i_y = position[i]
-            pos_j_x, pos_j_y = position[j]
-            distance = sqrt((pos_j_x - pos_i_x)^2 + (pos_j_y - pos_i_y)^2)
-            if distance < max_radius
-                sum_velocity_x += velocities[j][1]
-                sum_velocity_y += velocities[j][2]
+            neigh_x, neigh_y = state.boids[j]
+            vel_neigh_x, vel_neigh_y = state.vec_vel[j]
+            distance = hypot(neigh_x - boid_x, neigh_y - boid_y)
+            if distance < state.max_r
+                sum_velocity = (sum_velocity[1] + vel_neigh_x, sum_velocity[2] + vel_neigh_y)
                 neighbor_count += 1
             end
         end
     end
 
     if neighbor_count > 0
-        return (sum_velocity_x / neighbor_count, sum_velocity_y / neighbor_count)
+        avg_velocity_x = sum_velocity[1] / neighbor_count
+        avg_velocity_y = sum_velocity[2] / neighbor_count
+        accel_x = avg_velocity_x - vel_x
+        accel_y = avg_velocity_y - vel_y
+        return (accel_x, accel_y)
     else
-        return boids_velocity 
+        return (0.0, 0.0)
     end
 end
 
-# Функция для расчета центра масс
-function cohesion(state::WorldState, i::Int, max_radius::Int, factor_center::Float64)
-    sum_neigh_cord_x = 0.0
-    sum_neigh_cord_y = 0.0
+# Функция для расчета движения к центру
+function separation(state::WorldState, i::Int)
+    sum_neigh_coords=(0,0)
     neighbor_count = 0
-
-    position = state.boids
-
-    for j in 1:length(position)
+    boid_x, boid_y = state.boids[i]
+    
+    for j in 1:length(state.boids)
         if i != j
-            pos_i_x, pos_i_y = position[i]
-            pos_j_x, pos_j_y = position[j]
-            distance = sqrt((pos_j_x - pos_i_x)^2 + (pos_j_y - pos_i_y)^2)
-            if distance < max_radius
-                sum_neigh_cord_x += pos_j_x
-                sum_neigh_cord_y += pos_j_y
+            neigh_x, neigh_y = state.boids[j]
+            distance = hypot(neigh_x - boid_x, neigh_y - boid_y)
+            if distance < state.min_r
+                sum_neigh_coords = (sum_neigh_coords[1] + neigh_x, sum_neigh_coords[2] + neigh_y)
                 neighbor_count += 1
             end
         end
     end
 
     if neighbor_count > 0
-        center_x = sum_neigh_cord_x / neighbor_count
-        center_y = sum_neigh_cord_y / neighbor_count
-        return ((center_x - position[i][1]) * factor_center, (center_y - position[i][2]) * factor_center)
+         center_x = sum_neigh_coords[1] / neighbor_count 
+         center_y = sum_neigh_coords[2] / neighbor_count 
+         accel_x = -(center_x - boid_x) * state.factor_sep
+         accel_y = -(center_y - boid_y) * state.factor_sep
+        return (accel_x, accel_y)
     else
-        return state.velocities[i]
+        return (0.0, 0.0)
     end
 end
 
-
-# Функция отталкивания
-function separation(state::WorldState, i::Int, min_radius::Int, factor_separation::Float64)
-    sum_neigh_cord_x = 0.0
-    sum_neigh_cord_y = 0.0
+# Функция для расчета движения к центру
+function cohesion(state::WorldState, i::Int)
+    sum_neigh_coords=(0,0)
     neighbor_count = 0
-
-    position = state.boids
-
-    for j in 1:length(position)
+    boid_x, boid_y = state.boids[i]
+    
+    for j in 1:length(state.boids)
         if i != j
-            pos_i_x, pos_i_y = position[i]
-            pos_j_x, pos_j_y = position[j]
-            distance = sqrt((pos_j_x - pos_i_x)^2 + (pos_j_y - pos_i_y)^2)
-            if distance < min_radius
-                sum_neigh_cord_x += pos_j_x
-                sum_neigh_cord_y += pos_j_y
+            neigh_x, neigh_y = state.boids[j]
+            distance = hypot(neigh_x - boid_x, neigh_y - boid_y)
+            if distance < state.max_r
+                sum_neigh_coords = (sum_neigh_coords[1] + neigh_x, sum_neigh_coords[2] + neigh_y)
                 neighbor_count += 1
             end
         end
     end
 
     if neighbor_count > 0
-        center_x = sum_neigh_cord_x / neighbor_count
-        center_y = sum_neigh_cord_y / neighbor_count
-        return (-(center_x - position[i][1]) * factor_separation, -(center_y - position[i][2]) * factor_separation)
+         center_x = sum_neigh_coords[1] / neighbor_count 
+         center_y = sum_neigh_coords[2] / neighbor_count 
+         accel_x = (center_x - boid_x) * state.factor_center
+         accel_y = (center_y - boid_y) * state.factor_center
+        return (accel_x, accel_y)
     else
-        return state.velocities[i]
+        return (0.0, 0.0)
     end
 end
 
-# # Функция ограничения зоны движения
-# function keepWithinBounds(state::WorldState, i::Int, margin::Int)
-#     if (state.boids[i][1] > state.width - margin)
-#         state.velocities[i] = (-abs(state.velocities[i][1]+0.1), 
-#         state.velocities[i][2])
-#     end
-#     if (state.boids[i][1] <  margin)
-#         state.velocities[i] = (abs(state.velocities[i][1]+0.1), 
-#         state.velocities[i][2])
-#     end
-#     if (state.boids[i][2] > state.height - margin)
-#         state.velocities[i] = (state.velocities[i][1],
-#         -abs(state.velocities[i][2]+0.1))
-#     end
-#     if (state.boids[i][2] < margin)
-#         state.velocities[i] = (state.velocities[i][1],
-#         abs(state.velocities[i][2]+0.1))
-#     end
-# end
-
-# Функция для обновления состояния
+# Функция для обновления состояния boids
 function update!(state::WorldState)
-    max_radius = 10
-    min_radius = 3
-    factor_center = 0.1
-    factor_separation = 0.5
-    margin = 5
-
+  
     for i in 1:length(state.boids)
-        # Вычисляем компоненты новой скорости по каждому правилу
-        sep_x, sep_y = separation(state, i, min_radius, factor_separation)
-        align_x, align_y = alignment(state, i, max_radius)
-        coh_x, coh_y = cohesion(state, i, max_radius, factor_center)
-
-        # Суммируем компоненты для получения новой скорости
-        new_velocity_x = sep_x + align_x + coh_x
-        new_velocity_y = sep_y + align_y + coh_y
-        new_velocity = (new_velocity_x, new_velocity_y)
-
-        # Обновление позиции
+        new_vel = state.vec_vel[i]
         boid_x, boid_y = state.boids[i]
-        vel_x, vel_y = state.velocities[i]
-        new_position = (boid_x + vel_x, boid_y + vel_y)
+        
+        # Вызов функции alignment для расчета нового ускорения
+        align_x, align_y = alignment(state, i)
+        coh_x, coh_y = cohesion(state, i) 
+        sep_x, sep_y = separation(state, i)
 
-        # Проверка границ
-        # keepWithinBounds(state, i, margin)
+        accel_x= align_x + coh_x + sep_x
+        accel_y= align_y + coh_y + sep_y
 
-        # Обновление состояния
+        # Обновление скорости с учетом ускорения
+        new_vel = (new_vel[1] + accel_x, new_vel[2] + accel_y)
+
+        # Ограничение скорости
+        vel_x, vel_y = new_vel
+        speed = hypot(vel_x, vel_y)
+        if speed > state.max_vel
+           scale = state.max_vel / speed
+           new_vel = (vel_x * scale, vel_y * scale)
+        end
+
+        # Обновление позиции и проверка границ
+        new_position = (boid_x + new_vel[1], boid_y + new_vel[2])
         state.boids[i] = (mod(new_position[1], state.width), mod(new_position[2], state.height))
-        state.velocities[i] = new_velocity
+        state.vec_vel[i] = new_vel
     end
 end
 
-
-
-  function (@main)(ARGS)
-    h = 80
-    w = 80
-    n_boids = 10
-    state = WorldState(n_boids, h, w)
-    anim = @animate for time = 1:200
+# Функция для запуска анимации
+function main(ARGS)
+    h = 80.0
+    w = 80.0
+    n_boids = 80
+    max_r = 40
+    min_r = 2
+    max_vel = 1.0
+    factor_center = 0.15
+    factor_sep= 2.0
+    state = WorldState(n_boids, h, w, max_vel, min_r, max_r, factor_center, factor_sep)
+    anim = @animate for time in 1:200
         update!(state)
         boids = state.boids
-        scatter(boids, xlim = (0, state.width), ylim = (0, state.height), label=false)
+        scatter(boids, xlim=(0, state.width), ylim=(0, state.height), label=false)
     end
-    gif(anim, "boids.gif", fps = 10)
+    gif(anim, "boids.gif", fps=10)
 end
 
 end
 
+# Запуск модуля
 using .Boids
 Boids.main("")
